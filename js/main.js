76471ba373aaf35daef309785f2d7837e7d60465
kenyaramirez <kenyaramireztarazon@gmail.com>
2024-11-12 10:57:00 -0800

    Google analytics

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-C8THLPL8DP');
+
+
     // State management
     const state = {
         isLoggedIn: false,
